ba42476d1d4faf981471380b7b94631782e1e774
Teemu Kallio <teemu.kallio@native-instruments.de>
2017-07-07 14:32:14 +0200

    Express added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-const Chain = require('./chain');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Chain = require('./blockchain/chain');
+const config = require('./config');
 
 const blockchain = new Chain();
 
-blockchain.addBlock(blockchain.generateNextBlock('2nd block'));
-blockchain.addBlock(blockchain.generateNextBlock('third block'));
-blockchain.addBlock(blockchain.generateNextBlock('fourth block'));
+const app = express();
+app.use(bodyParser.json());
 
-console.log(blockchain.isValidChain());
-console.log(blockchain.blocks);
+app.get('/blocks', (req, res) => res.send(JSON.stringify(blockchain)));
+app.post('/add', (req, res) => {
+  const data = req.body.data;
+  const block = blockchain.generateNextBlock(data);
+
+  blockchain.addBlock(block);
+  res.send();
+});
+
+app.listen(config.HTTP_PORT, () => console.log(`Listening :${config.HTTP_PORT}...`));
